287edfd9a05afeae12410c98eed2da46007d9b01
Ashxen777 <ashxen7771@gmail.com>
2021-03-02 12:22:56 +0400

    Initial commit

---
+++ src/main/java/am/itspace/shoes_shoppe_store/ShoesShoppeStoreApplication.java
@@ -0,0 +1,13 @@
+package am.itspace.shoes_shoppe_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesShoppeStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoesShoppeStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/itspace/shoes_shoppe_store/ShoesShoppeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.shoes_shoppe_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesShoppeStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
